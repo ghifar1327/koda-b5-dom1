e3fa9b79fb6e99201e0fc950c650d910179437e0
ghifar1327 <givartorreto@gmail.com>
2025-11-25 14:12:04 +0700

    fix: fix typo

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const input = document.querySelector("form");
-const output = document.querySelectorAll(".output>div");
+const output = document.querySelectorAll(".output>td");
 input.addEventListener("submit", (e) => {
   e.preventDefault();
   //   const suhu = e.target.suhu.value;
-  output[0].textContent = `celcius = ${e.target.suhu.value}`;
-  output[1].textContent = `fahrenheit = ${(e.target.suhu.value * 9) / 5 + 32}`;
-  output[2].textContent = `kelvin = ${parseInt(e.target.suhu.value) + 273.15}`;
-  output[3].textContent = `reamur = ${(e.target.suhu.value * 4) / 5}`;
+  output[0].innerHTML = e.target.suhu.value
+  output[1].innerHTML = (e.target.suhu.value * 9) / 5 + 32
+  output[2].innerHTML = parseInt(e.target.suhu.value) + 273.15
+  output[3].innerHTML = (e.target.suhu.value * 4) / 5
 });
