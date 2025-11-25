d94564a99996640bf180628cd63871b735fafdb1
ghifar1327 <givartorreto@gmail.com>
2025-11-25 14:00:49 +0700

    feat: adds function celcius to kelvin conversion

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ input.addEventListener("submit", (e) => {
   //   const suhu = e.target.suhu.value;
   output[0].textContent = `celcius = ${e.target.suhu.value}`;
   output[1].textContent = `fahrenheit = ${(e.target.suhu.value * 9) / 5 + 32}`;
-
+  output[2].textContent = `kelvin = ${parseInt(e.target.suhu.value) + 273.15}`;
 });
